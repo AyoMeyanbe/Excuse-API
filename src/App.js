ddf70775f4caa26ae6e4db620f742a26effa8b06
AyoMeyanbe <ayomeyanbe@gmail.com>
2024-09-01 23:34:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios, { Axios } from "axios";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const fetchExcuse = (excuse) => {
+      axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
+      setText(res.data[0].excuse)
+    })
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Generate An Excuse</h1>
+      <button onClick={() => fetchExcuse("party")}>Party</button>
+      <button onClick={() => fetchExcuse("family")}>Family</button>
+      <button onClick={() => fetchExcuse("office")}>Office</button>
+      <p>{text}</p>
     </div>
   );
 }
